93c5f56babc4a1eb8294d5a722884b8647001586
SHIDQI ALHAKIM <kiminikt700@gmail.com>
2025-05-15 21:27:26 +0700

    input Sampah

--- Tubes.go
+++ Tubes.go
@@ -1,8 +1,25 @@
 package main
 
-type data struct {
+import "fmt"
+
+type dataSampah struct {
+	Jenis_Sampah  string
+	Jumlah_Sampah int
 }
 
-func main() {
+const NMAX int = 100
 
+type DataSampah [NMAX]dataSampah
+
+func main() {
+	var Data dataSampah
+	var nData int
+	fmt.Scan(&nData)
+}
+
+func InputSampah(A *dataSampah, n int) {
+	var i int
+	for i = 0; i <= n-1; i++ {
+		fmt.Scan(&A[i].Jenis_Sampah, &A[i].Jumlah_Sampah)
+	}
 }
